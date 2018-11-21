aeb863ff3e8d623c66c5905325e197e1dd98f62a
Alex <0alexgutierrez@gmail.com>
2018-11-20 23:19:59 -0700

    built tab navigator

---
+++ Root.js
@@ -0,0 +1,19 @@
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+
+import FrontPage from './screens/FrontPage';
+import Ask from './screens/Ask';
+import Show from './screens/Show';
+
+const TabNavigator = createBottomTabNavigator({
+  Home: {
+    screen: FrontPage
+  },
+  Ask: {
+    screen: Ask
+  },
+  Show: {
+    screen: Show
+  }
+});
+
+export default createAppContainer(TabNavigator);
