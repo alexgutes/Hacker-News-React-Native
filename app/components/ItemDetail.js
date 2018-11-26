ecaa7583829f6cc1f851f322265fab62c2719b69
Alex Gutierrez <0alexgutierrez@gmail.com>
2018-11-25 17:29:29 -0700

    added album list and header

---
+++ app/components/ItemDetail.js
@@ -0,0 +1,54 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+
+const urlParse = require('url-parse');
+
+class ItemDetail extends Component {
+  parseHost(url) {
+    if (url === null) {
+      return 'Hacker News';
+    }
+    const hostName = urlParse(url, true).hostname;
+    this.parts = hostName.split('.').slice(-3);
+    if (this.parts[0] === 'www') {
+      this.parts.shift();
+    }
+    return this.parts.join('.');
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Text>{this.props.item.title}</Text>
+        </CardSection>
+        <CardSection>
+          <Text>
+(
+            {this.parseHost(this.props.item.url)}
+)
+          </Text>
+        </CardSection>
+        <CardSection>
+          <Text>
+            {this.props.item.points}
+            {' '}
+points
+          </Text>
+        </CardSection>
+        <CardSection>
+          <Text>
+            {this.props.item.num_comments}
+            {' '}
+comments
+          </Text>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+export default ItemDetail;
--- app/screens/FrontPage.js
+++ app/screens/FrontPage.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import ItemDetail from '../components/ItemDetail';
+import Header from '../components/Header';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
-export default function FrontPage() {
-  return (
-    <View style={styles.container}>
-      <Text>FrontPage</Text>
-    </View>
-  );
+export default class FrontPage extends React.Component {
+  state = {
+    items: [],
+    // show: [],
+  };
+
+  componentWillMount() {
+    fetch('http://hn.algolia.com/api/v1/search?tags=front_page')
+      .then(response => response.json())
+      .then((response) => {
+        this.setState({ items: response.hits });
+      });
+  }
+
+  renderItems() {
+    const { items } = this.state;
+    return items.map(item => <ItemDetail key={item.objectID} item={item} />);
+  }
+
+  render() {
+    return (
+      <SafeAreaView>
+        <Header headerText="Albums" />
+        <ScrollView>{this.renderItems()}</ScrollView>
+      </SafeAreaView>
+    );
+  }
 }
